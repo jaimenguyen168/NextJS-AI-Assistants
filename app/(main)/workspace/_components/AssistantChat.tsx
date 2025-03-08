163b90e3337ab6bb0b90c8c7bedcb1f2a6becbcd
jaimenguyen168 <77992599+jaimenguyen168@users.noreply.github.com>
2025-03-08 01:12:28 -0500

    Get AI response

--- app/(main)/workspace/_components/AssistantChat.tsx
+++ app/(main)/workspace/_components/AssistantChat.tsx
@@ -5,12 +5,25 @@ import EmptyChatState from "@/app/(main)/workspace/_components/EmptyChatState";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Send } from "lucide-react";
+import { aiModelList } from "@/constants";
+import { AssistantContext } from "@/context/AssistantContext";
+import axios from "axios";
 
 const AssistantChat = () => {
+  const { currentAssistant } = useContext(AssistantContext);
   const [input, setInput] = useState("");
 
-  const onSendMessage = () => {
-    console.log("Send Message");
+  const onSendMessage = async () => {
+    const aiModel = aiModelList.find(
+      (item) => item.name === currentAssistant.aiModelId,
+    );
+
+    const result = await axios.post("api/eden-ai", {
+      provider: aiModel?.edenAi,
+      input,
+    });
+
+    setInput("");
   };
 
   return (
@@ -19,6 +32,7 @@ const AssistantChat = () => {
 
       <div className="flex justify-between px-12 gap-4 absolute left-0 right-0 bottom-6 w-full">
         <Input
+          value={input}
           placeholder="Start typing here..."
           onChange={(e) => setInput(e.target.value)}
           onKeyPress={(e) => e.key === "Enter" && onSendMessage()}
---
+++ app/api/eden-ai/route.tsx
@@ -0,0 +1,41 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const { provider, input } = await request.json();
+
+  const headers = {
+    Authorization: "Bearer " + process.env.EDEN_AI_API_KEY!,
+    "Content-Type": "application/json",
+  };
+  const url = "https://api.edenai.run/v2/multimodal/chat";
+  const body = JSON.stringify({
+    providers: [provider],
+    messages: [
+      {
+        role: "user",
+        content: [
+          {
+            type: "text",
+            content: {
+              text: input,
+            },
+          },
+        ],
+      },
+    ],
+  });
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers,
+    body,
+  });
+
+  const result = await response.json();
+  const res = {
+    role: "assistant",
+    content: result[provider].generated_text,
+  };
+
+  return NextResponse.json(res);
+}
